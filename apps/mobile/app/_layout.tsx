8f4d9f02cf7df9ac7332512b7881cde3a030378e
minthanhh <mint03sanzz@gmail.com>
2024-04-15 17:20:15 +0700

    feat(SplashScreen): adding demo plash screen

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -3,9 +3,13 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen'
-import { useEffect } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import Constants from 'expo-constants'
+import { Asset } from 'expo-asset'
 
 import { useColorScheme } from '@/shared/components/useColorScheme'
+import { Animated, StyleSheet } from 'react-native'
+import { View } from '@/shared/components/Themed'
 
 export {
     // Catch any errors thrown by the Layout component.
@@ -41,7 +45,96 @@ export default function RootLayout() {
         return null
     }
 
-    return <RootLayoutNav />
+    return (
+        <AnimatedAppLoader image={{ uri: Constants.expoConfig?.splash?.image }}>
+            <RootLayoutNav />
+        </AnimatedAppLoader>
+    )
+}
+
+function AnimatedAppLoader({ children, image }: AnimatedSplashScreenProps) {
+    const [isSplashReady, setSplashReady] = useState(false)
+
+    useEffect(() => {
+        async function prepare() {
+            await Asset.fromURI(image.uri!).downloadAsync()
+            setSplashReady(true)
+        }
+
+        prepare()
+    }, [image])
+
+    if (!isSplashReady) {
+        return null
+    }
+
+    return <AnimatedSplashScreen image={image}>{children}</AnimatedSplashScreen>
+}
+
+interface AnimatedSplashScreenProps {
+    children: React.ReactNode
+    image: { uri?: string }
+}
+
+function AnimatedSplashScreen({ children, image }: AnimatedSplashScreenProps) {
+    const animation = useMemo(() => new Animated.Value(1), [])
+    const [isAppReady, setAppReady] = useState(false)
+    const [isSplashAnimationComplete, setAnimationComplete] = useState(false)
+
+    useEffect(() => {
+        if (isAppReady) {
+            Animated.timing(animation, {
+                toValue: 0,
+                duration: 1000,
+                useNativeDriver: true,
+            }).start(() => setAnimationComplete(true))
+        }
+    }, [isAppReady])
+
+    const onImageLoaded = useCallback(async () => {
+        try {
+            await SplashScreen.hideAsync()
+            // Load stuff
+            await Promise.all([])
+        } catch (e) {
+            // handle errors
+        } finally {
+            setAppReady(true)
+        }
+    }, [])
+
+    return (
+        <View style={{ flex: 1 }}>
+            {isAppReady && children}
+            {!isSplashAnimationComplete && (
+                <Animated.View
+                    pointerEvents='none'
+                    style={[
+                        StyleSheet.absoluteFill,
+                        {
+                            backgroundColor: Constants.expoConfig?.splash?.backgroundColor,
+                            opacity: animation,
+                        },
+                    ]}>
+                    <Animated.Image
+                        style={{
+                            width: '100%',
+                            height: '100%',
+                            resizeMode: Constants.expoConfig?.splash?.resizeMode || 'contain',
+                            transform: [
+                                {
+                                    scale: animation,
+                                },
+                            ],
+                        }}
+                        source={image}
+                        onLoadEnd={onImageLoaded}
+                        fadeDuration={0}
+                    />
+                </Animated.View>
+            )}
+        </View>
+    )
 }
 
 function RootLayoutNav() {
